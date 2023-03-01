e20dfdb30cd6d676c5c7fa0906bb5417a8c19d6b
NRA <NRA@nowhere.com>
2023-03-01 11:52:57 +0100

    add calcbm_HC_hit_HL & viewit2

--- bfin/handler_stock_analysis.py
+++ bfin/handler_stock_analysis.py
@@ -191,6 +191,51 @@ def get_page_stock(ref,type="isin",last=-1,start="2021-01-01",PC_bearish=0.4,PC_
     )
     return (name,page,df,df_bm,df_bm_ana)
 
+def get_page_stock2(ref,type="isin",last=-1,start="2021-01-01",PC_bearish=0.4,PC_bullish=19,RSI_bearish=60,RSI_bullish=30,period_ana=90):
+    if type == "isin":
+        (name,c,df)=graph_isin_with_bourso(ref,last=last,start=start)
+    
+    #df_bm=calculate_best_moves(df,PC_bearish=PC_bearish,PC_bullish=PC_bullish,RSI_bearish=RSI_bearish,RSI_bullish=RSI_bullish,duration_limit=45)
+    # Add low limit to PC & RSI
+    df_bm=calcbm_HC_hit_HL(df,PC_bearish=PC_bearish,PC_bullish=PC_bullish,RSI_bearish=RSI_bearish,RSI_bullish=RSI_bullish,duration_limit=45)
+    df_bm_ana=analyse_best_moves(df=df,dfbm=df_bm,period=period_ana)
+    if len(df_bm_ana) > 0:
+        df_bm_ana_display=df_bm_ana[["date_buy","date_sell","best_date_sell","days","best_days","PC_buy","PC_sell","best_PC_sell","diffp","best_diffp","RSI_move","RSI","d-diffp"]]
+        df_bm_ana_display["date_sell"]=df_bm_ana_display["date_sell"].apply(lambda x:str(x).split(" ")[0])
+        df_bm_ana_display["date_buy"]=df_bm_ana_display["date_buy"].apply(lambda x:str(x).split(" ")[0])
+        df_bm_ana_display["best_date_sell"]=df_bm_ana_display["best_date_sell"].apply(lambda x:str(x).split(" ")[0])        
+    else:
+        df_bm_ana_display=pd.DataFrame()
+
+    if len(df_bm)>0:
+        df_bm_display = df_bm[df_bm.covered==0]
+        df_bm_display["date_sell"]=df_bm_display["date_sell"].apply(lambda x:str(x).split(" ")[0])
+        df_bm_display["date_buy"]=df_bm_display["date_buy"].apply(lambda x:str(x).split(" ")[0])
+        df_bm_display=df_bm_display.drop(columns=["year","covered"])
+    else:
+        df_bm_display=pd.DataFrame()
+    
+    page = Page(layout=Page.SimplePageLayout,page_title=str(ref)+":"+name)
+    table_color = '#333333'
+    table = (
+        Table()
+        .add(headers=list(df_bm_display.columns),rows=df_bm_display.values.tolist(),attributes={
+		"style": "background:{}; width:1500px; font-size:14px; color:#C0C0C0;padding:3px;text-align: center;".format(table_color)
+	    }).set_global_opts(title_opts=opts.ComponentTitleOpts(title="Best moves for "+name,title_style={"style":"color: white"})))
+    table_ana = (
+        Table()
+        .add(headers=list(df_bm_ana_display.columns),rows=df_bm_ana_display.values.tolist(),attributes={
+		"style": "background:{}; width:1500px; font-size:14px; color:#C0C0C0;padding:3px;text-align: center;".format(table_color)
+	    }).set_global_opts(title_opts=opts.ComponentTitleOpts(title="Analyse Best moves for "+name,title_style={"style":"color: white"})))
+
+    page.add(
+        c,
+        table,
+        table_ana
+    )
+    return (name,page,df,df_bm,df_bm_ana)
+
+
 # compute RSI
 def enrich_RSI(df):
     ## 14_Day RSI
@@ -282,6 +327,67 @@ def calculate_best_moves(df,PC_bullish=19,PC_bearish=0.1,RSI_bullish=30,RSI_bear
     else:
         return pd.DataFrame()
 
+# find best moves based on strategy:
+# Bullish = RSI < 30% && Price Channel HC% > 19%
+# Bearish = RSI > 70% && Price Channel HC% =~ 0%
+# duration_limit = max number of days to keep the stock (with this strategy after 90 days it s rarely usefull to keep it) 90 days = 60 Working days
+def calcbm_HC_hit_HL(df,PC_bullish=19,PC_bearish=0.1,RSI_bullish=30,RSI_bearish=70,duration
+    if ("PC_high-close%" in df.columns) & ("RSI" in df.columns):
+        df["diff_PCHC_HL"] = df.apply(lambda x: x["PC_high-low%"] - x["PC_high-close%"],axis=1)
+        ldiff=[]
+        for uc in df[(df["PC_high-close%"]>=PC_bullish) & (df["RSI"]<=RSI_bullish) & (df["diff_PCHC_HL"] < 1)].reset_index()["index"].to_list():
+            _d=df[((df.index == uc)) | ((df.index > uc) & (df["PC_high-close%"] <= PC_bearish) & (df["RSI"]>=RSI_bearish)) | ((df.index > uc + duration_limit)) ].head(2)
+            _e=_d.reset_index()
+            diff=_e.iloc[-1]["Close"]-_e.iloc[0]["Close"]
+            diffp=(_e.iloc[-1]["Close"]-_e.iloc[0]["Close"]) / _e.iloc[0]["Close"] * 100
+            days=(_e.iloc[-1]["Date"]-_e.iloc[0]["Date"]).days
+            ldiff.append({  "date_buy":_e.iloc[0]["Date"],
+                            "date_sell":_e.iloc[-1]["Date"] if len(_e) > 1 else None,
+                            "close_buy":_e.iloc[0]["Close"],
+                            "close_sell":_e.iloc[-1]["Close"] if len(_e) > 1 else None,
+                            "PC_buy":_e.iloc[0]["PC_high-close%"],
+                            "RSI_buy":_e.iloc[0]["RSI"],
+                            "PC_sell":_e.iloc[-1]["PC_high-close%"] if len(_e) > 1 else None,
+                            "diff":diff,
+                            "diffp":diffp,
+                            "days":days,
+                            "year":str(_e.iloc[0]["Date"]).split("-")[0]})
+
+        df_bm=pd.DataFrame(ldiff)
+        if len(df_bm) > 0:
+            df_bm["covered"]=0
+            df_bm=df_bm.reset_index().rename(columns={"index":"ind"})
+            # lookup unfinished move at the end
+            first_unfinish=df_bm[df_bm["date_sell"].isna()]["ind"].to_list()
+            if len(first_unfinish) > 0:
+                df_bm["covered"]=df_bm.apply(lambda x: 1 if (x["ind"] > first_unfinish[0]) else x["covered"],axis=1)
+            (u_index,u_row)=[x for x in df_bm[(df_bm.covered==0)].head(1).iterrows()][0]
+            while u_index is not None and u_row["date_sell"] is not None:
+                df_bm["covered"]=df_bm.apply(lambda x : 1 if ((x["ind"] > u_row["ind"]) & (x["date_buy"] <= u_row["date_sell"])) else x["covered"],axis=1)
+                try:
+                    (u_index,u_row)=[x for x in df_bm[(df_bm["covered"]==0)&(df_bm["ind"]>u_row["ind"])].head(1).iterrows()][0]
+                except:
+                    u_index = None
+            df_bm=df_bm.drop(columns=["ind"])
+            df_bm["RSI_buy"]=df_bm["RSI_buy"].apply(lambda x:int(x))
+            df_bm["diff"]=df_bm["diff"].apply(lambda x:round(x,2))
+            df_bm["diffp"]=df_bm["diffp"].apply(lambda x:round(x,2))
+            df_bm["Win/Day%"]=df_bm.apply(lambda x: int(x["diffp"]/x["days"]*365) if x["days"]>0 else 0,axis=1)
+            df_bm["Price_high"]=df_bm.apply(lambda x: df[(df["Date"]>x["date_buy"])&(df["Date"]<x["date_sell"])]["Close"].max() if x["date_sell"] is not None else df[df["Date"]>x["date_buy"]]["Close"].max(),axis=1) 
+            df_bm["Price_low"]=df_bm.apply(lambda x: df[(df["Date"]>x["date_buy"])&(df["Date"]<x["date_sell"])]["Close"].min() if x["date_sell"] is not None else df[df["Date"]>x["date_buy"]]["Close"].min(),axis=1) 
+            df_bm["Gain_high"]=df_bm.apply(lambda x: str(round(((x["Price_high"]-float(x["close_buy"]))/float(x["close_buy"]) * 100),2))+"%",axis=1)
+            df_bm["Gain_low"]=df_bm.apply(lambda x: str(round(((x["Price_low"]-float(x["close_buy"]))/float(x["close_buy"]) * 100),2))+"%",axis=1)
+            df_bm["RSI_max"]=df_bm.apply(lambda x: df[(df["Date"]>x["date_buy"])&(df["Date"]<x["date_sell"])]["RSI"].max() if x["date_sell"] is not None else df[df["Date"]>x["date_buy"]]["RSI"].max(),axis=1) 
+            df_bm["PC_HC_low"]=df_bm.apply(lambda x: df[(df["Date"]>x["date_buy"])&(df["Date"]<x["date_sell"])]["PC_high-close%"].min() if x["date_sell"] is not None else df[df["Date"]>x["date_buy"]]["PC_high-close%"].min(),axis=1) 
+            df_bm["RSI_max"]=df_bm["RSI_max"].fillna(0)
+            df_bm["RSI_max"]=df_bm["RSI_max"].apply(lambda x: int(x))
+
+        return df_bm#[df_bm["covered"]==0]
+    else:
+        return pd.DataFrame()
+
+
+
 # add a low indicator control to calculate_best_moves to avoid engaging on low PC_bullish (we keep 19 as a minimum even if the quantile give a lower value)
 def calculate_best_moves_low_limit(df,PC_bullish=19,PC_bearish=0.1,RSI_bullish=30,RSI_bearish=70,duration_limit=60):
     if PC_bullish < 21:
@@ -341,6 +447,26 @@ def viewit(isin,start="2021-01-01"):
     pRSI_Bullish=            int(df["RSI"].quantile(0.30))
     pRSI_Bearish=            int(df["RSI"].quantile(0.80))
     (n,p,df,dfbm,dfbm_ana)=get_page_stock(ref=isin,type="isin",start=start,PC_bearish=pPCHC_Bearish,PC_bullish=pPCHC_Bullish,RSI_bearish=pRSI_Bearish,RSI_bullish=pRSI_Bullish,period_ana=60)
+
+    print("pRSI_Bearish=",pRSI_Bearish," \t pRSI_Bullish=",pRSI_Bullish," \t pPCHC_Bearish=",pPCHC_Bearish, " \t pPCHC_Bullish=",pPCHC_Bullish)
+    if len(dfbm_ana) > 0:
+        print("DFBM ANA Gain:",dfbm_ana["best_diffp"].sum(),"DB BM Gain:",dfbm_ana["diffp"].sum())
+    else:
+        print("No move no Gain :(")
+    return p
+
+def viewit2(isin,start="2021-01-01"):
+    RSI_bearish=75
+    RSI_bullish=30
+    PC_bearish=0
+    PC_bullish=20
+
+    (n,p,df,dfbm,dfbm_ana)=get_page_stock2(ref=isin,type="isin",start=start,PC_bearish=PC_bearish,PC_bullish=PC_bullish,RSI_bearish=RSI_bearish,RSI_bullish=RSI_bullish,period_ana=60)
+    pPCHC_Bearish=int(df["PC_high-close%"].quantile(0.03))
+    pPCHC_Bullish=int(df["PC_high-close%"].quantile(0.87))
+    pRSI_Bullish=            int(df["RSI"].quantile(0.30))
+    pRSI_Bearish=            int(df["RSI"].quantile(0.80))
+    (n,p,df,dfbm,dfbm_ana)=get_page_stock2(ref=isin,type="isin",start=start,PC_bearish=pPCHC_Bearish,PC_bullish=pPCHC_Bullish,RSI_bearish=pRSI_Bearish,RSI_bullish=pRSI_Bullish,period_ana=60)
 
     print("pRSI_Bearish=",pRSI_Bearish," \t pRSI_Bullish=",pRSI_Bullish," \t pPCHC_Bearish=",pPCHC_Bearish, " \t pPCHC_Bullish=",pPCHC_Bullish)
     if len(dfbm_ana) > 0:
